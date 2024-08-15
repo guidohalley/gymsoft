54d4a4f0de1e92068bcd27edefd676de04412352
guidohalley <guidohalley@hotmail.com>
2024-08-15 12:38:04 -0300

    Refactorization filesystem

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const sequelize = require('./config/database');
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
